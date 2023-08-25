7777a71ecdc1267714f4b361e15938f4bf1951f2
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-08-25 14:37:23 -0300

    init portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,241 @@
-import Image from 'next/image'
+"use client";
+
+import ButtonTheme from "@/components/ui/button-theme";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+import brain from '../../public/brain.png'
+import my from '../../public/allan.jpeg'
+import Image from "next/image"
+
+import nestjs from '../../public/icons/nestjs.svg'
+import { Divider } from "@mui/material";
+
+const front = [
+  {
+    name: 'ReactJS'
+  },
+  {
+    name: 'NextJS'
+  },
+  {
+    name: 'Angular'
+  },
+  {
+    name: 'Flutter'
+  },
+  {
+    name: 'CSS'
+  },
+  {
+    name: 'HTML'
+  },
+  {
+    name: 'Javascript',
+  },
+  {
+    name: 'Bootstrap'
+  },
+  {
+    name: 'TailwindCSS'
+  },
+];
+
+const devops = [
+  {
+    name: 'Docker'
+  },
+  {
+    name: 'Kubernetes'
+  },
+  {
+    name: 'AWS'
+  },
+  {
+    name: 'Firebase'
+  },
+  {
+    name: 'Vercel'
+  },
+  {
+    name: 'Git'
+  },
+  {
+    name: 'Github'
+  },
+  {
+    name: 'Bitbucket'
+  },
+  {
+    name: 'Swagger'
+  }
+
+];
+
+const back = [
+  {
+    name: 'NodeJS',
+  },
+  {
+    name: 'NestJS',
+  },
+  {
+    name: 'Laminas',
+  },
+  {
+    name: 'ZendFramework',
+  },
+  {
+    name: 'Postgresql',
+  },
+  {
+    name: 'PrismaORM',
+  },
+  {
+    name: 'Jest',
+  },
+]
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main>
+        <section>
+          <div className="flex items-center justify-end">
+            <ButtonTheme />
+          </div>
+        </section>
+        <section className="my-10">
+          <Card className="p-10">
+            <div className="flex justify-center mb-5">
+              <Image
+                src={my}
+                alt="allan"
+                width={300}
+                height={300}
+                className="rounded-full opacity-50"
+              />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            </div>
+            <h1 className="text-center mb-10 text-3xl font-extrabold">
+              Hello! My name is Allan
+            </h1>
+            <Card className="p-5">
+              <p>
+                I am a highly skilled Full-stack Developer with four years of extensive experience in crafting cutting-edge web applications. My passion for web development is matched only by my enthusiasm for making a meaningful impact through technology. I possess a robust skill set that encompasses both front-end and back-end development, allowing me to create seamless, user-friendly web experiences.
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                Throughout my career, I have had the privilege of working on a diverse range of projects, from e-commerce platforms to content management systems and everything in between. My commitment to staying up-to-date with the latest industry trends and technologies ensures that I can deliver solutions that are not only functional but also innovative.
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                I thrive in collaborative environments and take pride in my ability to communicate effectively with cross-functional teams, turning ideas into reality. My dedication to clean code practices, performance optimization, and a user-centered approach is evident in the quality of my work.
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+                I am driven by a strong desire to contribute to the ever-evolving world of web development, and I am eager to take on new challenges and create exceptional digital experiences. Let's work together to bring your web projects to life and make a lasting impact in the digital realm.
+              </p>
+            </Card>
+          </Card>
+        </section>
+        <section className="card my-10">
+          <Card className="p-10">
+            <Card className="mx-5">
+              <div className="flex justify-center items-center ">
+                <Image
+                  src={brain}
+                  alt="brain"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </Card>
+            <div className="grid md:grid-cols-3 sm:grid-cols-1 md:gap-10 px-5">
+              <div className="flex flex-col">
+                <div className="flex justify-center">
+                  <Skeleton className="w-[10px] h-[100px]" />
+                </div>
+                <Card className="h-full">
+                  <h1 className="text-center py-5 text-lg font-bold">
+                    FrontEnd
+                  </h1>
+                  <Divider />
+                  <Timeline position="alternate">
+                    {front.map((tech, index) => (
+                      <TimelineItem key={index}>
+                        <TimelineSeparator>
+                          <TimelineDot>
+                          </TimelineDot>
+                          {index < front.length - 1 && <TimelineConnector />}
+                        </TimelineSeparator>
+                        <TimelineContent>{tech.name}</TimelineContent>
+                      </TimelineItem>
+                    ))}
+                  </Timeline>
+                </Card>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex justify-center">
+                  <Skeleton className="w-[10px] h-[100px]" />
+                </div>
+                <Card className="h-full">
+                  <h1 className="text-center py-5 text-lg font-bold">
+                    BackEnd
+                  </h1>
+                  <Divider />
+                  <Timeline position="alternate">
+                    {back.map((tech, index) => (
+                      <TimelineItem key={index}>
+                        <TimelineSeparator>
+                          <TimelineDot>
+                          </TimelineDot>
+                          {index < back.length - 1 && <TimelineConnector />}
+                        </TimelineSeparator>
+                        <TimelineContent>{tech.name}</TimelineContent>
+                      </TimelineItem>
+                    ))}
+                  </Timeline>
+                </Card>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex justify-center">
+                  <Skeleton className="w-[10px] h-[100px] relative" />
+                </div>
+                <Card className="h-full">
+                  <h1 className="text-center py-5 text-lg font-bold">
+                    DevOps
+                  </h1>
+                  <Divider />
+                  <Timeline position="alternate">
+                    {devops.map((tech, index) => (
+                      <TimelineItem key={index}>
+                        <TimelineSeparator>
+                          <TimelineDot>
+                          </TimelineDot>
+                          {index < devops.length - 1 && <TimelineConnector />}
+                        </TimelineSeparator>
+                        <TimelineContent>{tech.name}</TimelineContent>
+                      </TimelineItem>
+                    ))}
+                  </Timeline>
+                </Card>
+              </div>
+            </div>
+          </Card>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </section>
+      </main>
+    </>
   )
 }
